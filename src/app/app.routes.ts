7d72b527ef1df94a3d8f58c1a96cf3c7c0467e3f
BautistaSantosTapia <bautisantostapia04@gmail.com>
2026-04-09 18:15:02 -0300

    2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: "", },
-    {path: "login", },
-    {path: "registro", },
-    {path: "sobre-mi", },
-    {path: "**", },
+    {path: "login", loadComponent: ()=> import("./login/login").then((archivo)=> archivo.Login)},
+    {path: "registro", loadComponent: ()=> import("./registro/registro").then((archivo)=> archivo.Registro)},
+    {path: "sobre-mi", loadComponent: ()=> import("./sobre-mi/sobre-mi").then((archivo)=> archivo.SobreMi)},
+    {path: "", loadComponent: ()=> import("./bienvenida/bienvenida").then((archivo)=> archivo.Bienvenida), pathMatch: "full"},
+    {path: "**", loadComponent: ()=> import("./error/error").then((archivo)=> archivo.Error)},
 ];
